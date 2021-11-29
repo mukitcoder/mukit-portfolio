c2c47bc6332579b2b464aec5592e06a732012041
mukitcoder <mukitbd71@gmail.com>
2021-11-29 21:54:47 +0600

    add contact button type

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -4,7 +4,7 @@ import profileImage from "../images/mukit-01.png";
 
 const Contact = () => {
   return (
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-6 mb-16">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-6 mb-16">
       <div className="container mx-auto px-4">
         <Fade duration={4000}>
           {" "}
@@ -74,34 +74,59 @@ const Contact = () => {
           </div>
         </div>
       </div>
-    {/* send message area */}
+      {/* send message area */}
       <div>
-      <Fade duration={4000}>
+        <Fade duration={4000}>
           {" "}
           <h1 className="text-5xl text-center text-white py-2 mt-5">
             Send <span className="text-yellow-500">Me</span> Message
             <hr className="mt-4 w-2/3 mx-auto" />
           </h1>
         </Fade>
-        <section class="w-full text-gray-900 bg-center bg-cover bg-no-repeat mt-4">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 flex items-center justify-center">
-        <div class="p-8 flex flex-col lg:ml-auto w-full mt-10 lg:mt-0 rounded-md">
-            <div class="relative mb-4">
-                <label for="full-name" class="leading-7 text-sm text-gray-100">Name</label>
-                <input type="text" id="name" name="name" class="w-full bg-white rounded-md border border-gray-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-200 text-sm outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-150 ease-in-out"/>
-            </div>
-            <div class="relative mb-4">
-                <label for="email" class="leading-7 text-sm text-gray-100">Email</label>
-                <input type="email" id="email" name="email" class="w-full bg-white rounded-md border border-gray-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-200 text-sm outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-150 ease-in-out"/>
-            </div>
-            <div class="relative mb-4">
-                <label for="email" class="leading-7 text-sm text-gray-100">Message</label>
-                <textarea id="message" name="message" rows="4" class="w-full bg-white rounded-md border border-gray-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-200 text-sm outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-150 ease-in-out"> </textarea>
-            </div>
-            <button class="text-white font-bold bg-yellow-500 rounded-md border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 text-lg"><i class="fas fa-paper-plane mr-2"></i> Send</button>
-        </div>
-    </div>
-</section>
+        <section className="w-full text-gray-900 bg-center bg-cover bg-no-repeat mt-4">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 flex items-center justify-center">
+            <form className="p-8 flex flex-col lg:ml-auto w-full mt-10 lg:mt-0 rounded-md">
+              <div className="relative mb-4">
+                <label for="full-name" className="leading-7 text-sm text-gray-100">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  className="w-full bg-white rounded-md border border-gray-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-200 text-sm outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-150 ease-in-out"
+                />
+              </div>
+              <div className="relative mb-4">
+                <label for="email" className="leading-7 text-sm text-gray-100">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="w-full bg-white rounded-md border border-gray-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-200 text-sm outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-150 ease-in-out"
+                />
+              </div>
+              <div className="relative mb-4">
+                <label for="email" className="leading-7 text-sm text-gray-100">
+                  Message
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  rows="4"
+                  className="w-full bg-white rounded-md border border-gray-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-200 text-sm outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-150 ease-in-out"
+                >
+                  {" "}
+                </textarea>
+              </div>
+              <button className="text-white font-bold bg-yellow-500 rounded-md border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 text-lg" type="submit">
+                <i className="fas fa-paper-plane mr-2"></i> Send
+              </button>
+            </form>
+          </div>
+        </section>
       </div>
     </div>
   );
